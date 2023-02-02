fd8fdbd49971de6a02acf355fbfaeb0154add220
olivier <o.gozlan@ajc-ingenierie.fr>
2023-02-02 12:44:00 +0100

    config projet spring mvc

---
+++ quest-mvc/src/main/java/quest/controller/HomeController.java
@@ -0,0 +1,15 @@
+package quest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	//@RequestMapping=>Url qui permet d'arriver sur la methode
+	//le retour (String)=>la vue à afficher
+	@RequestMapping("/home")
+	public String home() {
+		return "/WEB-INF/views/home.jsp";
+	}
+}
